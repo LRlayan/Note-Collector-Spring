02e33c07e91a0c738b12738c05cda778e8f5344c
LRLayan <rameshlayan4@gmail.com>
2024-09-28 15:08:36 +0530

    modal mapping

--- src/main/java/com/example/springcrudpharse02notecollector/service/UserService.java
+++ src/main/java/com/example/springcrudpharse02notecollector/service/UserService.java
@@ -1,13 +1,12 @@
 package com.example.springcrudpharse02notecollector.service;
 
-import com.example.springcrudpharse02notecollector.dto.NoteDTO;
 import com.example.springcrudpharse02notecollector.dto.UserDTO;
-import com.example.springcrudpharse02notecollector.entity.impl.User;
+import com.example.springcrudpharse02notecollector.entity.impl.UserEntity;
 
 import java.util.List;
 
 public interface UserService {
-    NoteDTO saveUser(UserDTO userDTO);
+    UserDTO saveUser(UserDTO userDTO);
     List<UserDTO> getAllUser();
     boolean deleteUser(String id);
     boolean updateUser(String id,UserDTO userDTO);
