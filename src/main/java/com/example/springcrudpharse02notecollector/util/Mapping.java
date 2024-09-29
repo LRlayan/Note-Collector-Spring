48c2f477d6c5e7af7d05a33b029f80080c10a932
LRLayan <rameshlayan4@gmail.com>
2024-09-29 20:15:28 +0530

    userList()

--- src/main/java/com/example/springcrudpharse02notecollector/util/Mapping.java
+++ src/main/java/com/example/springcrudpharse02notecollector/util/Mapping.java
@@ -3,8 +3,10 @@ package com.example.springcrudpharse02notecollector.util;
 import com.example.springcrudpharse02notecollector.dto.UserDTO;
 import com.example.springcrudpharse02notecollector.entity.impl.UserEntity;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import java.util.List;
 
 @Component
 public class Mapping {
@@ -18,5 +20,9 @@ public class Mapping {
 
     public UserDTO toUserDTO(UserEntity userentity){
         return modelMapper.map(userentity,UserDTO.class);
+    }
+
+    public List<UserDTO> userList(List<UserEntity> userList){
+        return modelMapper.map(userList,new TypeToken<List<UserDTO>>(){}.getType()); // type token ek dmme list ekk ganna ona nisa.nttn eka user kenai enne.
     }
 }
