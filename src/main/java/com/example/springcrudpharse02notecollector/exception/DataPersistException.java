4c57f9e3f80c158923258c438c3b74fea43ef29a
LRLayan <rameshlayan4@gmail.com>
2024-09-30 10:34:34 +0530

    custom exception

---
+++ src/main/java/com/example/springcrudpharse02notecollector/exception/DataPersistException.java
@@ -0,0 +1,12 @@
+package com.example.springcrudpharse02notecollector.exception;
+
+public class DataPersistException extends RuntimeException{
+    public DataPersistException() {
+    }
+
+    public DataPersistException(String message) {
+    }
+
+    public DataPersistException(String message, Throwable cause) {
+    }
+}
