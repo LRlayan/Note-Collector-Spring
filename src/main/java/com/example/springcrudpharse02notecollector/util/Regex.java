2c9f1c0decd5d6a8026b92ff4a25031ae47b5dcc
LRLayan <rameshlayan4@gmail.com>
2024-09-30 21:19:17 +0530

    noteIdValidation()

--- src/main/java/com/example/springcrudpharse02notecollector/util/Regex.java
+++ src/main/java/com/example/springcrudpharse02notecollector/util/Regex.java
@@ -7,4 +7,8 @@ public class Regex {
     public static Matcher userIdValidate(String userId){
         return Pattern.compile("^USER-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$").matcher(userId);
     }
+
+    public static Matcher noteIdValidate(String noteId){
+        return Pattern.compile("^NOTE-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$").matcher(noteId);
+    }
 }
