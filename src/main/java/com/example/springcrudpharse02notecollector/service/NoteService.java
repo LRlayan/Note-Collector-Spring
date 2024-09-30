7d812ad0ac9ff5bc82e6d1817875ef7cdb476548
LRLayan <rameshlayan4@gmail.com>
2024-09-30 14:43:51 +0530

    refactor return type

--- src/main/java/com/example/springcrudpharse02notecollector/service/NoteService.java
+++ src/main/java/com/example/springcrudpharse02notecollector/service/NoteService.java
@@ -1,11 +1,11 @@
 package com.example.springcrudpharse02notecollector.service;
 
-import com.example.springcrudpharse02notecollector.dto.NoteDTO;
+import com.example.springcrudpharse02notecollector.dto.impl.NoteDTO;
 
 import java.util.List;
 
 public interface NoteService {
-    NoteDTO saveNote(NoteDTO noteDTO);
+    void saveNote(NoteDTO noteDTO);
     List<NoteDTO> getAllNote();
     boolean deleteNote(String id);
     boolean updateNote(String id,NoteDTO noteDTO);
