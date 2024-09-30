fddd0264668b39644d9641919183cce99e688d03
LRLayan <rameshlayan4@gmail.com>
2024-09-30 14:44:11 +0530

    saveNote()

--- src/main/java/com/example/springcrudpharse02notecollector/service/NoteServiceIMPL.java
+++ src/main/java/com/example/springcrudpharse02notecollector/service/NoteServiceIMPL.java
@@ -1,24 +1,31 @@
 package com.example.springcrudpharse02notecollector.service;
 
 import com.example.springcrudpharse02notecollector.dao.NoteDAO;
-import com.example.springcrudpharse02notecollector.dto.NoteDTO;
+import com.example.springcrudpharse02notecollector.dto.impl.NoteDTO;
+import com.example.springcrudpharse02notecollector.entity.impl.NoteEntity;
+import com.example.springcrudpharse02notecollector.exception.DataPersistException;
 import com.example.springcrudpharse02notecollector.util.Mapping;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 //meka service vlasse walata wisheshyen hadapu ekka.internally service walata galapena function meke implement karala tiyenawa.
 @Service
+@Transactional
 public class NoteServiceIMPL implements NoteService{
     @Autowired
-    Mapping mapping;
+    private Mapping mapping;
 
     @Autowired
-    NoteDAO noteDAO;
+    private NoteDAO noteDAO;
 
     @Override
-    public NoteDTO saveNote(NoteDTO noteDTO) {
-        return mapping.toNoteDTO(noteDAO.save(mapping.toNoteEntity(noteDTO)));
+    public void saveNote(NoteDTO noteDTO) {
+        NoteEntity noteSave = noteDAO.save(mapping.toNoteEntity(noteDTO));
+        if (noteSave == null){
+            throw new DataPersistException("Note not saved");
+        }
     }
 
     @Override
