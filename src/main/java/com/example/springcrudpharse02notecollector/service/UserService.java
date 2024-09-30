47261285e7c08daaddabf81f73c585310e8a5127
LRLayan <rameshlayan4@gmail.com>
2024-09-30 10:29:38 +0530

    refactor return type

--- src/main/java/com/example/springcrudpharse02notecollector/service/UserService.java
+++ src/main/java/com/example/springcrudpharse02notecollector/service/UserService.java
@@ -6,7 +6,7 @@ import com.example.springcrudpharse02notecollector.entity.impl.UserEntity;
 import java.util.List;
 
 public interface UserService {
-    UserDTO saveUser(UserDTO userDTO);
+    void saveUser(UserDTO userDTO);
     UserDTO getUser(String id);
     void deleteUser(String id);
     void updateUser(String userId,UserDTO userDTO);
