91c260662bb9048e5d9a68535df567620428259b
LRLayan <rameshlayan4@gmail.com>
2024-09-30 10:33:40 +0530

    save user validate

--- src/main/java/com/example/springcrudpharse02notecollector/service/UserServiceIMPL.java
+++ src/main/java/com/example/springcrudpharse02notecollector/service/UserServiceIMPL.java
@@ -3,12 +3,14 @@ package com.example.springcrudpharse02notecollector.service;
 import com.example.springcrudpharse02notecollector.dao.UserDAO;
 import com.example.springcrudpharse02notecollector.dto.UserDTO;
 import com.example.springcrudpharse02notecollector.entity.impl.UserEntity;
+import com.example.springcrudpharse02notecollector.exception.DataPersistException;
 import com.example.springcrudpharse02notecollector.util.Mapping;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -21,8 +23,17 @@ public class UserServiceIMPL implements UserService {
     private Mapping mapping;
 
     @Override
-    public UserDTO saveUser(UserDTO userDTO) {
-        return mapping.toUserDTO(userDAO.save(mapping.toUserEntity(userDTO)));
+    public void saveUser(UserDTO userDTO) {
+        // mapping.toUserDTO(userDAO.save(mapping.toUserEntity(userDTO)));
+        UserEntity saveUser = mapping.toUserEntity(userDTO);
+        if (saveUser == null){
+            throw new DataPersistException("User Not Saved.Please Try Again.");
+        }
+        //hibernate currpt data save kranne nh.
+        //app ekak validation karana than tiyeno front-end eke
+        //dao eketh krno
+        //controller eketh krnawa.
+        //meka service eke karata dao ekata thama adala wenne.meke dao layer eka simplyfy interface ekk witri tiyenne.e nisa mekata auwa me validation ek krnna wenne service eke.
     }
 
     @Override
@@ -38,8 +49,16 @@ public class UserServiceIMPL implements UserService {
     }
 
     @Override
-    public void updateUser(String id, UserDTO userDTO) {
-        mapping.toUserDTO(userDAO.save(mapping.toUserEntity(userDTO)));
+    public void updateUser(String userId, UserDTO userDTO) {
+        Optional<UserEntity> tmpUser = userDAO.findById(userId);
+        if (tmpUser.isPresent()){
+            tmpUser.get().setFirstName(userDTO.getFirstName());
+            tmpUser.get().setLastName(userDTO.getLastName());
+            tmpUser.get().setEmail(userDTO.getEmail());
+            tmpUser.get().setPassword(userDTO.getPassword());
+            tmpUser.get().setProfilePicture(userDTO.getProfilePicture());
+            //jpa wala update method ekk nh native wala thamai tiyenne update method.
+        }
     }
 
     @Override
