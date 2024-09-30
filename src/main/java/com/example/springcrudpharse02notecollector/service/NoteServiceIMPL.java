e7348ae8f00095309e6423c38e06dc39bbc20e4f
LRLayan <rameshlayan4@gmail.com>
2024-09-30 10:34:04 +0530

    save note

--- src/main/java/com/example/springcrudpharse02notecollector/service/NoteServiceIMPL.java
+++ src/main/java/com/example/springcrudpharse02notecollector/service/NoteServiceIMPL.java
@@ -1,18 +1,24 @@
 package com.example.springcrudpharse02notecollector.service;
 
+import com.example.springcrudpharse02notecollector.dao.NoteDAO;
 import com.example.springcrudpharse02notecollector.dto.NoteDTO;
-import com.example.springcrudpharse02notecollector.util.AppUtil;
+import com.example.springcrudpharse02notecollector.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 //meka service vlasse walata wisheshyen hadapu ekka.internally service walata galapena function meke implement karala tiyenawa.
 @Service
 public class NoteServiceIMPL implements NoteService{
+    @Autowired
+    Mapping mapping;
+
+    @Autowired
+    NoteDAO noteDAO;
 
     @Override
     public NoteDTO saveNote(NoteDTO noteDTO) {
-        noteDTO.setNoteId(AppUtil.generateNoteId());
-        return noteDTO;
+        return mapping.toNoteDTO(noteDAO.save(mapping.toNoteEntity(noteDTO)));
     }
 
     @Override
