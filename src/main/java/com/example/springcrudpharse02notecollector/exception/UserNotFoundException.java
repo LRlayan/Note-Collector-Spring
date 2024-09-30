50bd42c4106c0549b1a64da2ffe32931734d157c
LRLayan <rameshlayan4@gmail.com>
2024-09-30 13:06:14 +0530

    custom userException

---
+++ src/main/java/com/example/springcrudpharse02notecollector/exception/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.springcrudpharse02notecollector.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+
+    }
+
+    public UserNotFoundException(String message) {
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+    }
+}
