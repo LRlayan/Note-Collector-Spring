15d0c4933a10fe1c28d7186295edd9dd540f3fea
LRLayan <rameshlayan4@gmail.com>
2024-09-30 15:15:05 +0530

    NoteNotFoundException

---
+++ src/main/java/com/example/springcrudpharse02notecollector/exception/NoteNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.springcrudpharse02notecollector.exception;
+
+public class NoteNotFoundException extends RuntimeException{
+    public NoteNotFoundException() {
+
+    }
+
+    public NoteNotFoundException(String message) {
+    }
+
+    public NoteNotFoundException(String message, Throwable cause) {
+    }
+}
