14298859475bf29dfed8e373a8e4f770d15ae022
LRLayan <rameshlayan4@gmail.com>
2024-10-03 20:20:19 +0530

    refactor comment

--- src/main/java/com/example/springcrudpharse02notecollector/controller/HealthCheckController.java
+++ src/main/java/com/example/springcrudpharse02notecollector/controller/HealthCheckController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/health")
 public class HealthCheckController {
-    //serama end point up unada blanna apita health controller ekak hadanna pluwan.
-
     @GetMapping
     public String healthTest(){
         return "Server is working";
