cc22090e5dc2e0796753a3421c81b7dd432e70a6
LRLayan <rameshlayan4@gmail.com>
2024-10-04 16:12:43 +0530

    Add Annotation

--- src/main/java/com/example/springcrudpharse02notecollector/dao/NoteDAO.java
+++ src/main/java/com/example/springcrudpharse02notecollector/dao/NoteDAO.java
@@ -2,6 +2,8 @@ package com.example.springcrudpharse02notecollector.dao;
 
 import com.example.springcrudpharse02notecollector.entity.impl.NoteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NoteDAO extends JpaRepository<NoteEntity,String> {
 }
